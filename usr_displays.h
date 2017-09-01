2879437114642c8c6fb61f2901ca4c89edc51c90
blue-7 <blueseven914@gmail.com>
2017-09-01 20:07:02 +0900

    Modify unused functions

--- usr_displays.h
+++ usr_displays.h
@@ -14,12 +14,12 @@ std::vector<DisplayIF*>	GetUsrDisplays()
 //	iDisplays.push_back( new Display_SSD1306_i2c(180,0) );
 //	iDisplays.push_back( new Display_SSD1306_i2c(180,2) );	// for SH1306
 
-//	iDisplays.push_back( new Display_ILI9341_spi_TM24(270,67) );
+//	iDisplays.push_back( new Display_ILI9341_spi_TM24(270) );
 //	iDisplays.push_back( new Display_ILI9341_spi_TM22(270) );
 //	iDisplays.push_back( new Display_ILI9341_spi_TM22(0) );
 	iDisplays.push_back( new Display_ILI9328_spi_TM22(270));
-//	iDisplays.push_back( new Display_ILI9225_spi(270));//,198) );
-//	iDisplays.push_back( new Display_ST7735_spi(90,3) );
+//	iDisplays.push_back( new Display_ILI9225_spi(270) );
+//	iDisplays.push_back( new Display_ST7735_spi(90) );
 
 	return	iDisplays;
 }
