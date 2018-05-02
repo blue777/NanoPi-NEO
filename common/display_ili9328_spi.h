7761af2f5410d7ba3163a74724dd6276b80d668a
blue-7 <blueseven914@gmail.com>
2018-05-03 00:20:41 +0900

    Support ili9486, fbdev

--- common/display_ili9328_spi.h
+++ common/display_ili9328_spi.h
@@ -140,7 +140,7 @@ public:
 	}
 
 protected:
-	virtual	int		TransferRGB565( int x, int y, int cx, int cy, const uint8_t * image )
+	virtual	int		TransferRGB(int x, int y, int cx, int cy, const uint8_t * image, int image_bytes )
 	{
 		int		ret;
 		uint8_t	WriteDataCmd[3]	= { 0x70, 0x00, 0x22 };
